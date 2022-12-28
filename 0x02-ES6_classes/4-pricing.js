456fa13dabf723497c68e261e8d047b0b0f25a59
Ruby <graceffiong@gmail.com>
2022-12-28 20:52:57 +0100

    remove import

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -21,7 +21,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.Currency.displayFullCurrency()}`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
