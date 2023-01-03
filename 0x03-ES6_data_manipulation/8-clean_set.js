d339bf5f6da92611eee69b58fbb3b63c88e92e51
Ruby <graceffiong@gmail.com>
2023-01-03 08:42:13 +0100

    add conditions in task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,9 @@
 const cleanSet = (set, startString) => {
   const contained = [];
 
-  if (startString.length === 0) return '';
+  if (startString.length === 0
+    || typeof set !== 'object'
+    || typeof startString !== 'string') return '';
 
   for (const item of set) {
     if (item && item.startsWith(startString)) {
