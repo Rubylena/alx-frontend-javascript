d708ef462e0f10e8b3daf12670f8b68d0a207462
Ruby <graceffiong@gmail.com>
2023-01-11 12:26:03 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
     [key: string]: any;
 }
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
 // example
 const teacher3: Teacher = {
     firstName: 'John',
@@ -17,3 +21,13 @@ const teacher3: Teacher = {
   };
   
   console.log(teacher3);
+
+// example
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
