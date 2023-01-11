1f414b871d208bb3de8cd87b3bfeda40fe4e488d
Ruby <graceffiong@gmail.com>
2023-01-11 13:55:47 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -31,3 +31,40 @@ const director1: Directors = {
     numberOfReports: 17,
   };
   console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string,
+  lastName: string): string
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string ) : string =>
+  `${firstName[0]}. ${lastName}`;
+
+console.log(printTeacher("John", "Doe"));
+
+interface StudentClassInterface {
+  firstName: string;
+  lastname: string;
+}
+
+interface StudentClassConstructor {
+  new(firstName: string, lastname: string): StudentClassInterface
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastname: string;
+
+  constructor(firstName: string, lastName: string){
+    this.firstName = firstName;
+    this.lastname = lastName;
+  }
+
+  workOnHomework(): string{
+    return "Currently working";
+  }
+
+  displayname():string{
+    return this.firstName;
+  }
+}
